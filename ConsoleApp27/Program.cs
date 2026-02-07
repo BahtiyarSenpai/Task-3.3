c7b430813ab084aabfdcb2434b804838f0509dd4
Student <sunshbaha@gmail.com>
2026-02-07 18:19:37 +0500

    1

--- ConsoleApp27/Program.cs
+++ ConsoleApp27/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+
+public class BahaSenpai
+{  
+    public int TripleAttack(int damageOne, int damageTwo, int damageThree)
+    {
+        return damageOne + damageTwo + damageThree;
+    }
+}
